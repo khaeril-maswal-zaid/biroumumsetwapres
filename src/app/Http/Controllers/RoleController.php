75ec3ef20954e630229bdbd91ee975d0a4cf464d
Khaeril Maswal Zaid <muhammadkhaerilzadi@gmail.com>
2026-03-12 17:21:10 +0700

    Make user for Upin Ipin

--- src/app/Http/Controllers/RoleController.php
+++ src/app/Http/Controllers/RoleController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Models\MasterPegawai;
 use App\Models\User;
-use Illuminate\Support\Facades\Hash;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Str;
@@ -17,42 +16,6 @@ class RoleController extends Controller
 {
     public function index()
     {
-        $data = [
-            [
-                'name' => 'Gladys Anindya Trisvitanica Putri',
-                'email' => 'gladys.atp@set.wapresri.go.id',
-                'nip' => 'MagangSwp001',
-                'biro' => '0239',
-                'jabatan' => 'Magang',
-            ],
-            [
-                'name' => 'Kyza Aulia',
-                'email' => 'kyza.aulia@set.wapresri.go.id',
-                'nip' => 'MagangSwp002',
-                'biro' => '0239',
-                'jabatan' => 'Magang',
-            ],
-        ];
-
-        foreach ($data as $key => $value) {
-            User::create([
-                'nip' => $value['nip'],
-                'nip_sso' => $value['nip'],
-                'is_ldap' => 0,
-                'email' => $value['email'],
-                'password' => Hash::make($value['nip']),
-            ]);
-
-            MasterPegawai::create([
-                'nip' => $value['nip'],
-                'name' => $value['name'],
-                'kode_unit' => '02',
-                'kode_biro' => $value['biro'],
-                'jabatan' => $value['jabatan'],
-            ]);
-        }
-
-        die;
         $kodeUnit = Auth::user()?->pegawai?->kode_unit;
 
         $users = User::with(['roles', 'pegawai.unit'])
